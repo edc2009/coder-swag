29db636c8abc94f4f3f863402dc3a7974a0cd4cd
Christopher Gibson <chris.edc.2010@gmail.com>
2018-11-06 14:41:35 -0800

    new project

---
+++ coder-swag/coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Christopher on 11/6/18.
+//  Copyright © 2018 Christopher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
